b175e8527c35fffccc3363290a2f6351baf73414
Johann Heuer <johann.f.heuer@gmail.com>
2024-10-30 22:22:30 +0100

    app controller scaffold

---
+++ src/controller/app_controller.py
@@ -0,0 +1,12 @@
+from model.app_model import AppModel
+
+
+class AppController:
+    """
+    ### `AppController`
+
+    Manages the **Application State** `AppModel`.
+    """
+
+    def __init__(self, app_model: AppModel):
+        pass
