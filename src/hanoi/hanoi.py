d754d1b36c8f8af053935d7d182dbc79afd638e5
Johann Heuer <johann.f.heuer@gmail.com>
2024-10-31 01:51:45 +0100

    refactor move to standard class instead of typed dict to use methods

--- src/hanoi/hanoi.py
+++ src/hanoi/hanoi.py
@@ -6,11 +6,11 @@ def towers_of_hanoi(
     n: int, source: int, destination: int, auxiliary: int
 ) -> List[Move]:
     if n == 1:
-        return [{"source": source, "destination": destination}]
+        return [Move(source, destination)]
     moves: List[Move] = []
 
     moves += towers_of_hanoi(n - 1, source, auxiliary, destination)
-    moves.append({"source": source, "destination": destination})
+    moves.append(Move(source, destination))
     moves += towers_of_hanoi(n - 1, auxiliary, destination, source)
 
     return moves
--- src/hanoi/move.py
+++ src/hanoi/move.py
@@ -1,6 +1,13 @@
 from typing import TypedDict
 
 
-class Move(TypedDict):
+class Move:
     source: int
     destination: int
+
+    def __init__(self, source: int, destination: int):
+        self.source = source
+        self.destination = destination
+
+    def __str__(self) -> str:
+        return f"{self.source} -> {self.destination}"
