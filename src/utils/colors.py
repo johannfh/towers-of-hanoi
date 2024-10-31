f0e181abfc142c36bdbc7ac80bff98ab2fdcecc4
Johann Heuer <johann.f.heuer@gmail.com>
2024-10-31 02:00:57 +0100

    more color utils

--- src/utils/colors.py
+++ src/utils/colors.py
@@ -1,4 +1,26 @@
+from typing import List
 import pygame
+
+def generate_gradient(
+    origin: pygame.Color, target: pygame.Color, n: int
+) -> List[pygame.Color]:
+    assert n >= 0, "n must not be negative"
+    if n == 0:
+        return []
+    if n == 1:
+        return [origin]
+
+    step = 1 / (n - 1)
+    return [origin.lerp(target, step * i) for i in range(0, n)]
+
+def invert(color: pygame.Color) -> pygame.Color:
+    """Inverts a color, e.g. `#000000` -> `#ffffff`"""
+
+    return pygame.Color(
+        r=255 - color.r,
+        g=255 - color.g,
+        b=255 - color.b,
+    )
 
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
